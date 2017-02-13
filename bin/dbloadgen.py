d95165288b53e6c3d4485db0e9f1c32f64b19e63
Bipul Kumar <bipulc@yahoo.com>
2017-02-13 17:09:32 +0000

    Initial version

---
+++ bin/dbloadgen.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python
+''' Light weight toolkit to generate Database Load
+    To be used for comparison of Oracle DB performance across two instances
+    Developed and Tested using Oracle 12c
+'''
+
+
+import sys, os, argparse, getpass, time, logging, cx_Oracle, threading
+sys.path.append('../lib')
+import helper
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-d", type=str, help="database Name from TNSnames.ora or LDAP")
+parser.add_argument("-u", type=str, help="username")
+parser.add_argument("-w", type=int, default=2, help="number of write threads")
+parser.add_argument("-r", type=int, default=2, help="number of read threads")
+parser.add_argument("-l", type=str, help="logfile (fullpath)")
+parser.add_argument("-t", type=int, default=1, help="elapsed time to run the test in minutes")
+#parser.add_argument("-a", type=str, default='y', help="take awr snapshot")
+parser.add_argument("-n", type=str, help="generate network workload", nargs='?')
+args = parser.parse_args()
+
+# Prompt for password
+password = getpass.getpass('password: ')
+
+username = args.u
+database = args.d
+numth_write = args.w
+numth_read = args.r
+logfile = args.l
+test_duration = args.t
+#gen_awr = args.a
+gen_network_load = args.n
+
+# Set logging and print name of logfile to check
+loglevel="INFO"
+nloglevel =getattr(logging, loglevel, None)
+helper.t_logsetting(logfile, nloglevel)
+print ("\nMain: execution information in logfile %s " %logfile)
+helper.t_log('\n')
+helper.t_log('Thread| Main : Starting')
+helper.t_log('Thread| Main : ' + 'DB Load Gen Arguments:')
+helper.t_log('Thread| Main : ' + 'Num writer thread: %s' %numth_write)
+helper.t_log('Thread| Main : ' + 'Num reader thread: %s' %numth_read)
+helper.t_log('Thread| Main : ' + 'Test Run Duration (min): %s' %test_duration)
+
+# Collect pre load gen Stats
+
+helper.t_log('Thread| Main : ' + 'Collect pre load statistics')
+preloadstats = helper.Statsretreiver(username,password,database)
+prestats =  preloadstats.getStats()
+
+# Start Writer Threads
+
+writerthreads = []
+
+for i in range(numth_write):
+    threadname = 'Thread ' + str(i)
+    tw = threading.Thread(target=helper.runWriterProc, args=(threadname, username, password, database, test_duration))
+    writerthreads.append(tw)
+    tw.start()
+
+# Start Reader Threads
+
+readerthreads = []
+
+for i in range(numth_read):
+    threadname = 'Thread ' + str(i)
+    tr = threading.Thread(target=helper.runReaderProc, args=(threadname, username, password, database, test_duration))
+    readerthreads.append(tr)
+    tr.start()
+
+if numth_write > 0:
+    tw.join()
+if numth_read > 0:
+    tr.join()
+
+# Collect post load gen Stats
+
+helper.t_log('Thread| Main : ' + 'Collect post load statistics')
+postloadstats = helper.Statsretreiver(username,password,database)
+poststats = postloadstats.getStats()
+
+# Compare the Stats and print perf report
+
+helper.t_log('Thread| Main : ' + 'Resource Consumption Summary \n')
+for metric in prestats.keys():
+    val = poststats[metric] - prestats[metric]
+    helper.t_log('Thread| Main : %s : %i' %(metric,val))
+
+helper.t_log('Thread| Main : Completed')
